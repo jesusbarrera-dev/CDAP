0613d44a1a60726931dc1308145f1f40bbaf9ce4
Alex Palacios <jesus.pbarrera@alumnos.udg.mx>
2020-10-28 19:38:35 -0600

    Configurado routes y carpeta public

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,20 @@
+const path = require('path');
+const ejs = require('ejs');
+
+const routes = require('../routes/index.js');
+
 module.exports = app => {
 
   //settings
   app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+
+  //static files
+  app.use('/public', express.static(path.join(__dirname, '../public')));
+
+  //routes
+  routes(app);
 
   return app;
 }
