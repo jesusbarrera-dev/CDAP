fd055266d83804fb9678d832971489d3ec209c2a
Alex Palacios <jesus.pbarrera@alumnos.udg.mx>
2020-10-30 23:16:26 -0600

    Informacion y estilos iniciales a la pagina de contacto agregados

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,22 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = app => {
-  
-  
+
+
   app.get('/contacto', (req, res) => {
-    console.log(__dirname)
     res.render("contact");
   });
-  
+
   app.get('/SobreNosotros', (req, res) => {
     res.render("aboutUs");
   });
-  
+
   app.get('/', (req, res) => {
     res.render("indexu");
   });
-  
-  app.use((req, res,next) => {
+
+  app.use((req, res, next) => {
     res.status(404).render("error404")
   });
 
