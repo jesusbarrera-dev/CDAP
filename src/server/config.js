41e83df8c148e0126e43fe9c3f04460f4f1899b1
Alex Palacios <jesus.pbarrera@alumnos.udg.mx>
2020-11-24 10:50:43 -0600

    Conexion a base de datos local realizada

--- src/server/config.js
+++ src/server/config.js
@@ -7,7 +7,7 @@ const routes = require('../routes/index.js');
 module.exports = app => {
 
   //settings
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3001);
   app.set('views', path.join(__dirname, '../views'));
   app.set('view engine', 'ejs');
 
